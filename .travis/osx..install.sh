e64601fc1ab7b724bba73d74dd7fe379cba84351
Tal Lancaster <tal.lancaster+gh@gmail.com>
2017-01-14 22:20:21 -0700

    travis setup for osx and Qt5 builds

---
+++ .travis/osx..install.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+PACKAGES="cmake qt5"
+ 
+brew reinstall $PACKAGES
---
+++ .travis/osx..script.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# force qt5 build
+# Workaround; No FindQt5.cmake module exists
+export CMAKE_PREFIX_PATH="$(brew --prefix qt5)"
+
+cmake ..
