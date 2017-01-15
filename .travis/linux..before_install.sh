adfd52dc70b0bbcdc4ec7abed5de005b9327703d
Tal Lancaster <tal.lancaster+gh@gmail.com>
2017-01-15 16:19:47 -0700

    travis: for now not building against clang under linux

--- .travis/linux..before_install.sh
+++ .travis/linux..before_install.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
 sudo add-apt-repository ppa:ubuntu-sdk-team/ppa -y
+
+# Qt 5.7
+sudo add-apt-repository ppa:beineri/opt-qt571-trusty -y
+
 sudo apt-get update -qq
+sudo apt-get install -qq qt57base
