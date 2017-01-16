b94713b009008b6c43275a6750c13b64505f40f1
Tal Lancaster <tal.lancaster+gh@gmail.com>
2017-01-15 20:18:48 -0700

    linux..before_install.sh: usint qt57 repo

--- .travis/linux..before_install.sh
+++ .travis/linux..before_install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 sudo add-apt-repository ppa:ubuntu-sdk-team/ppa -y
+sudo add-apt-repository --yes ppa:beineri/opt-qt57-trusty
+
 sudo apt-get update -qq
-sudo apt-get install -y qt5-default
+sudo apt-get install -y qt57-meta-full
