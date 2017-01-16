a8ce6aad783aef02d73ced13ba959b83e9b89c96
Tal Lancaster <tal.lancaster+gh@gmail.com>
2017-01-15 20:23:39 -0700

    linux..before_install.sh: apt-get -qq instead of -y

--- .travis/linux..before_install.sh
+++ .travis/linux..before_install.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
 sudo add-apt-repository ppa:ubuntu-sdk-team/ppa -y
+
+# Qt 5.7
 sudo add-apt-repository --yes ppa:beineri/opt-qt57-trusty
 
 sudo apt-get update -qq
-sudo apt-get install -y qt57-meta-full
+sudo apt-get install -qq qt57-meta-full
