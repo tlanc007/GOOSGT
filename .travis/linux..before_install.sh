ab70361b0443f085543ff7ecdccb1b2a4d2d407e
Tal Lancaster <tal.lancaster+gh@gmail.com>
2017-01-15 21:28:37 -0700

    fixed apt get call

--- .travis/linux..before_install.sh
+++ .travis/linux..before_install.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-sudo add-apt-repository ppa:ubuntu-sdk-team/ppa -y
+sudo apt-get-repository ppa:ubuntu-sdk-team/ppa -y
 
 # Qt 5.7
-sudo add-apt-repository ppa:beineri/opt-qt571-trusty -y
+sudo apt-get-repository ppa:beineri/opt-qt571-trusty -y
 
 sudo apt-get update -qq
-sudo apt-get install -qq qt57base;  source /opt/qt57/bin/qt57-env.sh; 
+sudo apt-get install -qq qt57base;  source /opt/qt57/bin/qt57-env.sh;
