c71970e1c35df543c2a40e53a23ad14a863ddada
Tal Lancaster <tal.lancaster+gh@gmail.com>
2017-01-16 19:39:21 -0700

    travis: still trying to fix no dir build error

--- .travis/linux..before_script.sh
+++ .travis/linux..before_script.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-
+cd "${TRAVIS_BUILD_DIR}"
+cmake . -Bbuild -DCMAKE_CXX_COMPILER="$CXX" -DCMAKE_C_COMPILER="$CC" -DCMAKE_BUILD_TYPE=$BUILD_TYPE
 cmake --build build -- -j$JOBS
--- .travis/osx..before_script.sh
+++ .travis/osx..before_script.sh
@@ -3,5 +3,7 @@
 # force qt5 build
 # Workaround; No FindQt5.cmake module exists
 export CMAKE_PREFIX_PATH="$(brew --prefix qt5)"
+cd "${TRAVIS_BUILD_DIR}"
+cmake . -Bbuild -DCMAKE_CXX_COMPILER="$CXX" -DCMAKE_C_COMPILER="$CC" -DCMAKE_BUILD_TYPE=$BUILD_TYPE
 cmake --build build -- -j$JOBS
 
